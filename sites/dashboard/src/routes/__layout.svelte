85e091cd764008b3c98355a2d123840a6ae3f51e
Jan Hein Hoogstad <yeehaa@offcourse.io>
2021-12-05 22:45:17 +0100

    wip: website

---
+++ sites/dashboard/src/routes/__layout.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	import '../app.css';
+</script>
+
+<main>
+	<div class="container ">
+		<slot />
+	</div>
+</main>
+
+<style lang="postcss">
+	.container {
+		@apply max-w-4xl mx-auto py-8;
+	}
+</style>
