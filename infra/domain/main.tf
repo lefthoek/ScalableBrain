daa2f2c5fe6c75c63c3251262a48402ef6287c3d
Jan Hein Hoogstad <yeehaa@offcourse.io>
2021-12-12 22:13:29 +0100

    again...

--- infra/domain/main.tf
+++ infra/domain/main.tf
@@ -12,14 +12,13 @@ variable "root_domain_name" {}
 
 
 resource "aws_route53_zone" "zone" {
-  provider = aws.us
   name = var.root_domain_name
 }
 
 resource "aws_route53_record" "validation" {
   provider = aws.us
   for_each = {
-    for dvo in aws.us_certificate.certificate.domain_validation_options : dvo.domain_name => {
+    for dvo in aws_acm_certificate.certificate.domain_validation_options : dvo.domain_name => {
       name   = dvo.resource_record_name
       record = dvo.resource_record_value
       type   = dvo.resource_record_type
@@ -35,7 +34,7 @@ resource "aws_route53_record" "validation" {
 }
 
 
-resource "aws.acm_certificate" "certificate" {
+resource "aws_acm_certificate" "certificate" {
   provider = aws.us
   domain_name               = var.root_domain_name
   validation_method         = "DNS"
@@ -45,9 +44,9 @@ resource "aws.acm_certificate" "certificate" {
   }
 }
 
-resource "aws.acm_certificate_validation" "default" {
+resource "aws_acm_certificate_validation" "default" {
   provider = aws.us
-  certificate_arn = aws.us_certificate.certificate.arn
+  certificate_arn = aws_acm_certificate.certificate.arn
   validation_record_fqdns = [for record in aws_route53_record.validation : record.fqdn]
   timeouts {
     create = "60m"
@@ -63,5 +62,5 @@ output "zone_id" {
 }
 
 output "certificate_arn" {
-  value = aws.us_certificate.certificate.arn
+  value = aws_acm_certificate.certificate.arn
 }
