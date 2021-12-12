6b4b81f9d14d2b7eb4fe89e483ccd66d547655dc
Jan Hein Hoogstad <yeehaa@offcourse.io>
2021-12-12 01:04:07 +0100

    for each

--- infra/domain/main.tf
+++ infra/domain/main.tf
@@ -15,12 +15,23 @@ resource "aws_route53_zone" "zone" {
 }
 
 resource "aws_route53_record" "validation" {
-  zone_id = aws_route53_zone.zone.zone_id
-  name = aws_acm_certificate.certificate.domain_validation_options.0.resource_record_name
-  type = aws_acm_certificate.certificate.domain_validation_options.0.resource_record_type
-  records = aws_acm_certificate.certificate.domain_validation_options.0.resource_record_value
+  for_each = {
+    for dvo in aws_acm_certificate.certificate.domain_validation_options : dvo.domain_name => {
+      name   = dvo.resource_record_name
+      record = dvo.resource_record_value
+      type   = dvo.resource_record_type
+    }
+  }
+
+  allow_overwrite = true
+  name            = each.value.name
+  records         = [each.value.record]
+  ttl             = 60
   ttl = "300"
+  type            = each.value.type
+  zone_id = aws_route53_zone.zone.zone_id
 }
+
 
 resource "aws_acm_certificate" "certificate" {
   provider = aws.acm
