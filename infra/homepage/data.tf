0447fce91fa7510640523de5fb621d71c7ac9ed6
Jan Hein Hoogstad <yeehaa@offcourse.io>
2021-12-18 22:58:34 +0100

    add env vars

--- infra/homepage/data.tf
+++ infra/homepage/data.tf
@@ -1,14 +1,14 @@
 data "aws_iam_policy_document" "public_read" {
   statement {
     actions = ["s3:GetObject"]
+    principals {
+      type        = "*"
+      identifiers = ["*"]
+    }
 
     resources = [
       "arn:aws:s3:::${var.root_domain_name}/*"
     ]
 
-    principals {
-      type        = "*"
-      identifiers = ["*"]
-    }
   }
 }
