836d5159d9fa6d11d79dca4a8e679080c76ceeb8
Jan Hein Hoogstad <yeehaa@offcourse.io>
2021-12-18 23:49:51 +0100

    revert back to inline policy

--- infra/homepage/context.tf
+++ infra/homepage/context.tf
@@ -11,7 +11,7 @@ variable "certificate_arn" {}
 variable "zone_id" {}
 
 locals {
-  module_name      = "home"
+  module_name      = "homepage"
   resources_prefix = "${var.project_name}-${var.environment_name}-${local.module_name}"
   parameter_prefix = "/${var.project_name}/${var.environment_name}/${local.module_name}"
   root_bucket      = var.root_domain_name
