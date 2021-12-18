6a9ec57ca2ff43fc6664d0510885757bba4daef7
Jan Hein Hoogstad <yeehaa@offcourse.io>
2021-12-19 00:03:24 +0100

    add distribution id

--- infra/homepage/parameters.tf
+++ infra/homepage/parameters.tf
@@ -3,3 +3,9 @@ resource "aws_ssm_parameter" "homepage_bucket" {
   type  = "SecureString"
   value = local.root_bucket
 }
+
+resource "aws_ssm_parameter" "homepage_distribution_d" {
+  name  = "${local.parameter_prefix}/distribution/id"
+  type  = "SecureString"
+  value = aws.cloudfront_distribution.root_distribution.id
+}
