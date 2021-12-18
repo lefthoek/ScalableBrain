62c822997eabb38d2c633bda8019d8e402d95a6e
Jan Hein Hoogstad <yeehaa@offcourse.io>
2021-12-18 21:32:18 +0100

    accept automatic certificate validation doesn't work

--- infra/domain/main.tf
+++ infra/domain/main.tf
@@ -5,32 +5,10 @@ resource "aws_route53_zone" "zone" {
   name = var.root_domain_name
 }
 
-resource "aws_route53_record" "validation" {
-  for_each = {
-    for dvo in aws_acm_certificate.certificate.domain_validation_options : dvo.domain_name => {
-      name   = dvo.resource_record_name
-      record = dvo.resource_record_value
-      type   = dvo.resource_record_type
-    }
-  }
-
-  allow_overwrite = true
-  name            = each.value.name
-  records         = [each.value.record]
-  ttl             = 60
-  type            = each.value.type
-  zone_id         = aws_route53_zone.zone.zone_id
-}
-
-
 resource "aws_acm_certificate" "certificate" {
-  domain_name       = var.root_domain_name
-  validation_method = "DNS"
-}
-
-resource "aws_acm_certificate_validation" "default" {
-  certificate_arn         = aws_acm_certificate.certificate.arn
-  validation_record_fqdns = [for record in aws_route53_record.validation : record.fqdn]
+  domain_name               = "*.${var.root_domain_name}"
+  validation_method         = "EMAIL"
+  subject_alternative_names = [var.root_domain_name]
 }
 
 output "domain_name" {
