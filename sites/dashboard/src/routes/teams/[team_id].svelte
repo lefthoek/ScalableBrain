531cbd3ece1720e4acb2d9e75a587fbc5f44e806
Jan Hein Hoogstad <yeehaa@offcourse.io>
2021-12-19 21:17:48 +0100

    add teams page

--- sites/dashboard/src/routes/teams/[team_id].svelte
+++
@@ -1,7 +0,0 @@
-<script>
-	import { page } from '$app/stores';
-	const { team_id } = $page.params;
-</script>
-
-<h1 class="text-8xl">{team_id}</h1>
-<pre>{JSON.stringify($page.params, null, 2)}</pre>
