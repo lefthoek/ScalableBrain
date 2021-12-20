ae4811d0b58e528b4facd38c96687335bc0cc99d
Jan Hein Hoogstad <yeehaa@offcourse.io>
2021-12-20 01:06:05 +0100

    just very basic

---
+++ sites/homepage/src/components/Button.svelte
@@ -0,0 +1,25 @@
+<script lang="typescript">
+  export let type = "button";
+  export let href = "";
+  export let target = "__blank";
+</script>
+
+{#if href}
+  <a {target} class="button" {href}><slot /></a>
+{:else}
+  <button class="button" {type}>
+    <slot />
+  </button>
+{/if}
+
+<style lang="postcss">
+  .button {
+    @apply py-2 px-4 bg-red-500 text-white font-semibold rounded-lg shadow-md;
+  }
+  .button:hover {
+    @apply bg-purple-700;
+  }
+  .button:focus {
+    @apply outline-none ring-2 ring-purple-400 ring-opacity-75;
+  }
+</style>
---
+++ sites/homepage/src/components/TeamImportStats.svelte
@@ -0,0 +1,18 @@
+<script lang="typescript">
+  const type = "button";
+  const params = new URLSearchParams(window.location.search);
+  const team_id = params.get("team_id");
+</script>
+
+<div class="widget" {type}>
+  <h1 class="team_id">Team ID: {team_id}</h1>
+</div>
+
+<style lang="postcss">
+  .team_id {
+    @apply text-8xl;
+  }
+  .widget {
+    @apply p-8 bg-neutral-900 text-gray-100;
+  }
+</style>
