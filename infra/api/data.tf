0f7512e187f30720a9b2de7c092d83db444fdf88
Jan Hein Hoogstad <yeehaa@offcourse.io>
2021-12-21 23:31:19 +0100

    fix typo

--- infra/api/data.tf
+++ infra/api/data.tf
@@ -1,3 +1,13 @@
+data "aws_iam_policy_document" "lambda_role_assume_role_policy" {
+  statement {
+    actions = ["sts:AssumeRole"]
+
+    principals {
+      type        = "Service"
+      identifiers = ["lambda.amazonaws.com"]
+    }
+  }
+}
 data "aws_iam_policy_document" "websockets_tables_access" {
   statement {
     actions = [
