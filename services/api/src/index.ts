23d6bfaf9fab7dfc837e2a6fb32a8532dd7e738e
Jan Hein Hoogstad <yeehaa@offcourse.io>
2021-12-22 00:22:03 +0100

    fake handler

--- services/api/src/index.ts
+++ services/api/src/index.ts
@@ -4,3 +4,16 @@ import app from "./graphql";
 const proxy = awsLambdaFastify(app);
 
 exports.graphql = proxy;
+
+const {
+  WS_CONNECTIONS_TABLE: connections,
+  WS_SUBSCRIPTIONS_TABLE: subscriptions,
+} = process.env;
+
+exports.subscriptions = () => {
+  console.log(connections, subscriptions);
+  return {
+    statusCode: 200,
+    body: JSON.stringify(process.env, null, 2),
+  };
+};
