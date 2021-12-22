c6ed08ca3f0f1db570df4f71568659d686fbe6e5
Jan Hein Hoogstad <yeehaa@offcourse.io>
2021-12-22 20:23:55 +0100

    add urql

--- sites/homepage/src/components/TeamImportStats.svelte
+++ sites/homepage/src/components/TeamImportStats.svelte
@@ -2,6 +2,14 @@
   const type = "button";
   const params = new URLSearchParams(window.location.search);
   const team_id = params.get("team_id");
+
+  import { createClient, setClient } from "@urql/svelte";
+
+  const client = createClient({
+    url: "https://bdaey0v9lf.execute-api.eu-west-1.amazonaws.com/dev/graphql",
+  });
+
+  setClient(client);
 </script>
 
 <div class="widget" {type}>
