219f253e63819c86fb830642a89380c09f2e6644
Jan Hein Hoogstad <yeehaa@offcourse.io>
2022-01-02 00:33:47 +0100

    convert event

--- services/api/src/handlers/bridge.ts
+++ services/api/src/handlers/bridge.ts
@@ -1,9 +1,24 @@
+import { LefthoekEvent } from "@lefthoek/types/dist/events";
+import { Handler as AWSHandler } from "aws-lambda";
 import { subscriptionServer } from "./websockets";
 
-export const bridge = (event: any) => {
+const wrapper: (handler: any) => AWSHandler = (handler) => {
+  return async (awsEvent, _context) => {
+    const detail = awsEvent.detail;
+    const detailType = awsEvent["detail-type"];
+    await handler({
+      detail,
+      detailType,
+    });
+  };
+};
+
+const _bridge = (event: LefthoekEvent) => {
   console.log(event);
   return subscriptionServer.publish({
-    topic: event["detail-type"],
+    topic: event.detailType,
     payload: event.detail.team,
   });
 };
+
+export const bridge = wrapper(_bridge);
