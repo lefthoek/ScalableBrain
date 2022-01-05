be80480d967ba75815808ce55637687febcdfe04
Jan Hein Hoogstad <yeehaa@offcourse.io>
2022-01-05 23:28:18 +0100

    change auth column names

--- services/api/src/handlers/oauth/index.ts
+++ services/api/src/handlers/oauth/index.ts
@@ -2,15 +2,14 @@ import fetch from "node-fetch";
 import { EventBridge } from "@lefthoek/adapters";
 import { AuthLookup } from "@lefthoek/stores";
 import { ProviderType, LefthoekEventType } from "@lefthoek/types";
-import type { SlackOAuthData, Team, TeamProvider } from "@lefthoek/types";
+import type { SlackOAuthData } from "@lefthoek/types";
 import type { SlackOAuthQueryString } from "./types";
 import { v4 as uuid } from "uuid";
-
-const { TEAM_ADDED } = LefthoekEventType;
 
 const {
   SLACK_CLIENT_SECRET: client_secret,
   SLACK_CLIENT_ID: client_id,
+  AUTH_LOOKUP_TABLE: table_name,
   HANDLER_NAME: handler_name,
   EVENT_BUS_NAME: event_bus_name,
 } = process.env;
@@ -19,12 +18,15 @@ export const slack = async (event: SlackOAuthQueryString) => {
   const eventBus = new EventBridge({ handler_name, event_bus_name });
   const authLookup = new AuthLookup({ table_name });
   const baseURL = "https://slack.com/api/oauth.v2.access";
+  const params = new URLSearchParams({ team_id: id });
+  const location = `https://zwarmer.com/teams/?${params.toString()}`;
+  const oauthURL = `${baseURL}?client_id=${client_id}&client_secret=${client_secret}&code=${code}`;
   const { code } = event.queryStringParameters;
+  const id = uuid();
+
   try {
-    const oauthURL = `${baseURL}?client_id=${client_id}&client_secret=${client_secret}&code=${code}`;
     const response = await fetch(oauthURL);
     const { access_token, team } = (await response.json()) as SlackOAuthData;
-    const id = uuid();
     const { name, id: provider_id } = team;
     const provider_type = ProviderType.Slack;
 
@@ -36,19 +38,14 @@ export const slack = async (event: SlackOAuthQueryString) => {
       access_token,
     });
 
-    const detail: Team = {
-      id,
-      name: team.name,
-      providers: [{ type: provider_type, id: provider_id, name }],
-    };
-
     await eventBus.put({
-      detailType: TEAM_ADDED,
-      detail,
+      detailType: LefthoekEventType.TEAM_ADDED,
+      detail: {
+        id,
+        name: team.name,
+        providers: [{ type: provider_type, id: provider_id, name }],
+      },
     });
-
-    const params = new URLSearchParams({ team_id: id });
-    const location = `https://zwarmer.com/teams/?${params.toString()}`;
 
     return {
       statusCode: 301,
