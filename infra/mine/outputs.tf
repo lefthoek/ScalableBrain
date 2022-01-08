99019e63d6bfd7b32b0866c4c47a85a5b81a3deb
Jan Hein Hoogstad <yeehaa@offcourse.io>
2022-01-09 00:24:05 +0100

    FIX: acces team bucket

--- infra/mine/outputs.tf
+++ infra/mine/outputs.tf
@@ -1,3 +1,3 @@
-output "raw_data_bucket_write_access_policy" {
-  value = aws_iam_policy.raw_data_bucket_write_access.arn
+output "raw_data_bucket_read_access_policy" {
+  value = aws_iam_policy.raw_data_bucket_read_access.arn
 }
