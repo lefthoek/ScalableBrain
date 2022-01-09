d71653fcc5152a8a6fcbf188952fef89938f592d
Jan Hein Hoogstad <yeehaa@offcourse.io>
2022-01-09 21:56:22 +0100

    add wildcard to bridge

--- sites/homepage/src/components/TeamImportStats.svelte
+++ sites/homepage/src/components/TeamImportStats.svelte
@@ -1,6 +1,8 @@
 <script lang="ts">
   export let team_id: string;
+
   import { operationStore, query, subscription } from "@urql/svelte";
+  import JSONTree from "svelte-json-tree";
 
   const teamData = operationStore(
     `
@@ -36,35 +38,50 @@
   query(teamData);
 </script>
 
-{#if $teamData.data}
-  <h1 class="text-8xl font-serif mb-8 text-skin-base dark:text-skin-inverted">
-    {$teamData.data.team.name}
-  </h1>
+<div class="space-y-8">
   <div>
-    <h2>
-      Lefthoek Id:
-      <span>{$teamData.data.team.id}</span>
-    </h2>
-    <h2>Providers:</h2>
-    {#each $teamData.data.team.providers as { type, id }}
-      <ul>
-        <li>{type}: {id}</li>
-      </ul>
-    {/each}
+    {#if $teamData.data}
+      <h1
+        class="text-8xl font-serif mb-8 text-skin-base dark:text-skin-inverted"
+      >
+        {$teamData.data.team.name}
+      </h1>
+      <div>
+        <h2>
+          Lefthoek Id:
+          <span>{$teamData.data.team.id}</span>
+        </h2>
+        <h2>Providers:</h2>
+        {#each $teamData.data.team.providers as { type, id }}
+          <ul class="list-disc list-inside">
+            <li>{type}: {id}</li>
+          </ul>
+        {/each}
+      </div>
+    {:else}
+      <h1
+        class="text-4xl font-serif mb-8 text-skin-base dark:text-skin-inverted"
+      >
+        {team_id}
+      </h1>
+    {/if}
   </div>
-{:else}
-  <h1 class="text-4xl font-serif mb-8 text-skin-base dark:text-skin-inverted">
-    {team_id}
-  </h1>
-{/if}
 
-<h2>Messages:</h2>
-{#if !$messages.data}
-  <p>No New Messages</p>
-{:else}
-  <ul>
-    {#each $messages.data as message}
-      <li><pre>{JSON.stringify(message, null, 2)}</pre></li>
-    {/each}
-  </ul>
-{/if}
+  <div class="space-y-4">
+    <h2 class="text-4xl">Messages</h2>
+    {#if !$messages.data}
+      <p>No New Messages</p>
+    {:else}
+      <ul>
+        {#each $messages.data as message}
+          <li>
+            <span class="block font-semibold">
+              {message.detailType}
+            </span>
+            <JSONTree value={JSON.parse(message.detail)} />
+          </li>
+        {/each}
+      </ul>
+    {/if}
+  </div>
+</div>
