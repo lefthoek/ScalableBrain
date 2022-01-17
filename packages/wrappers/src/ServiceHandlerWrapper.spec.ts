0a74af93ba477e837da7e005b24e9324d4eeafc5
Jan Hein Hoogstad <yeehaa@offcourse.io>
2022-01-17 23:59:59 +0100

    working specs, need refactoring

--- packages/wrappers/src/ServiceHandlerWrapper.spec.ts
+++ packages/wrappers/src/ServiceHandlerWrapper.spec.ts
@@ -18,20 +18,28 @@ describe("ServiceHandlerWrapper", () => {
   });
 
   it("puts an event on the bus with a valid handler", async () => {
-    const handler = async () => {
-      return {
-        detailType: "SOMETHING",
-        detail: {},
-      };
+    const eventBusReply = { success: true };
+    const event = {
+      detailType: "SOMETHING...",
+      detail: {},
     };
-    const cb = jest.fn();
-    const wrapper = serviceHandlerWrapper(handler);
-    await wrapper({ "detail-type": "SOMETHING...", detail: {} }, null, cb);
-    expect(cb).toHaveBeenCalledWith(null, undefined);
+    mock.mockResolvedValue(eventBusReply);
+    handlerMock.mockResolvedValue(event);
+    const wrapper = serviceHandlerWrapper(handlerMock);
+    await wrapper(
+      { "detail-type": event.detailType, detail: event.detail },
+      null,
+      cbMock
+    );
+    expect(handlerMock).toHaveBeenCalledWith(event, expect.anything());
+    expect(mock).toBeCalledWith(event);
+    expect(cbMock).toHaveBeenCalledWith(null, eventBusReply);
   });
 });
 
 const mock = jest.fn();
+const handlerMock = jest.fn();
+const cbMock = jest.fn();
 
 jest.mock("@lefthoek/adapters", () => ({
   __esModule: true, // this property makes it work
